baf1ddeeb65276c7f8fd60ff0885bb5d79c8e467
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-10-17 17:53:22 +0530

    disabled linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,24 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      // Disable all ESLint rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/ban-types": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/display-name": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "prefer-const": "off",
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "no-var": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
